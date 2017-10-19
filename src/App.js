4cbb61d0d988290358cde4c8c448a7d0c80f825d
George Song <george@monozuku.com>
2017-10-19 03:49:18 -0700

    Presentation related changes

--- src/App.js
+++ src/App.js
@@ -5,16 +5,11 @@ import Footer from './components/Footer';
 import Message from './components/Message';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
-import logo from './logo.svg';
 
 import './App.css';
 
 export default () => (
   <div className="App">
-    <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>Welcome to React with Redux</h2>
-    </div>
     <Router>
       <div className="Todo-App">
         <Message />
